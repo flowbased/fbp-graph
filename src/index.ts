a5c935ca8da6617c89f71e18de97de4c2d135f6c
Henri Bergius <henri.bergius@iki.fi>
2020-12-03 11:18:01 +0100

    Fix exporting of Journal and Graph

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,10 @@
 import * as journal from './Journal';
 import * as graph from './Graph';
 
-const { Journal } = journal;
-const { Graph } = graph;
-
 export {
   journal,
   graph,
-  Journal,
-  Graph,
 };
+
+export { Journal } from './Journal';
+export { Graph } from './Graph';
